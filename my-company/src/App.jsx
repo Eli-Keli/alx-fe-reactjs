80e119b981fd446f60215c6afed344db0aeb200b
Eli Keli <muthokaeli@gmail.com>
2024-08-16 19:57:39 +0300

    move Contact to ./components

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
-import Contact from './Contact';
+import Contact from './components/Contact';
 import Services from './Services';
 import Navbar from './components/Navbar';
 
