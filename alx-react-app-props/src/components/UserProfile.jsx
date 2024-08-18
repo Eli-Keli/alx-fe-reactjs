5569cc7fb4327fa0a9079059da785fcaad2bae18
Eli Keli <muthokaeli@gmail.com>
2024-08-18 15:11:09 +0300

    Modified - Add UserContext

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,11 @@
 import UserInfo from './UserInfo';
+import UserContext from './UserContext';
+import { useContext } from 'react';
 
 function ProfilePage() {
-  return <UserInfo />;
+  const userData = useContext(UserContext)
+
+  return <UserInfo userData={userData} />;
 }
 
 export default ProfilePage;
